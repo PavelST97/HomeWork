692844bf2c11f0f0327a9f0ac7fe2c749ec90de1
Pavel <strong973@mail.ru>
2021-01-13 16:19:55 +0300

    task_5_1

---
+++ task_5_1.py
@@ -0,0 +1,21 @@
+# Написать генератор песенки «TEN green bottles hanging on the wall».
+# В песенке вместо цифр должны быть именно слова,
+# обозначающие цифры, т.е. TEN, NINE, EIGHT, etc.
+# Песенка состоит из серии куплетов вот таких:
+# <количество> green bottles hanging on the wall,
+# <количество> green bottles hanging on the wall,
+# And if one green bottle should accidentally fall,
+# There‛ll be...
+# <пустая строка>
+# <количество - 1> green bottles hanging on the wall...
+# И так до тех пор, пока не закончатся бутылки. Когда
+# они закончились, то песенка должна закончится
+# фразой: NO green bottles hanging on the wall!
+
+number_for_song = ['TEN', 'NINE', 'EIGHT', 'SEVEN', 'SIX', 'FIVE', 'FOUR', 'THREE', 'TWO', 'ONE']
+
+for i in range(11):
+    if i < 10:
+        print(f'{number_for_song[i]} green bottles hanging on the wall,')
+    else:
+        print('NO green bottles hanging on the wall!')
