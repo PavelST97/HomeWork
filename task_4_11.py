fb07e5d82de787ec4aabc89baf7fc4bc987dd238
Pavel <strong973@mail.ru>
2021-01-15 18:12:03 +0300

    add new way

--- task_4_11.py
+++ task_4_11.py
@@ -2,12 +2,19 @@
 
 
 my_list = [1, 2, 3, 4, 5, 6]
-
 i = 0
 summ = 0
+summ2 = 0
+
 while i < len(my_list):
     if my_list[i] % 2 == 0:
         summ += 1
     i += 1
 
 print('колличество четных чисел в списке = ', summ)
+
+for el in my_list:
+    if el % 2 == 0:
+        summ2 += 1
+
+print('Колличество четных чисел в списке = ', summ2)
